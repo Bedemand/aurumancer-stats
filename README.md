2af57d4667fdeffcb0bd17caec3c2f54e2d8b658
Bade <bade@example.com>
2026-04-02 22:19:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aurumancer-stat-keeper
+
+A PWA for keeping track of resources for my board-game Aurumancer
---
+++ sw.js
@@ -0,0 +1,4 @@
+// sw.js - A basic empty service worker
+self.addEventListener('install', function(e) {
+  console.log('Service Worker installed');
+});
